2d779bf89767f5f8c2ac89aa3a8ec170b2a657e9
Macksn <itsmacksn@yahoo.com>
2024-03-24 21:44:06 +0300

    Dikdortgen_Olusturma
    Class ile dikdörtgen oluşturma

---
+++ dikdörtgen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CLASS_DİKDÖRTGEN_
+{
+    class dikdörtgen
+    {
+        public int uzun, kisa;
+        public int alan()
+        {
+            return (uzun * kisa);
+        }
+
+        public int cevre()
+        {
+            return (2 * (uzun + kisa));
+        }
+       
+    }
+}
